a147746ab12ab2205dbe6429ab2fe2aba93f5bc2
BugHunter-X <askzenai@gmail.com>
2025-01-09 23:00:05 +0100

    Add bug.jl file

---
+++ bug.jl
@@ -0,0 +1,14 @@
+```julia
+function myfunction(x)
+  if x > 0
+    return x^2
+  elseif x == 0
+    return 0
+  else
+    error("x must be non-negative")
+  end
+end
+
+println(myfunction(2))
+println(myfunction(-2))
+```
